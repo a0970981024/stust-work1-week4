ba8b8158898e6228a9dc012b88ab057b0d38b9a9
日平 趙 <4b1g0034@office.stust.edu.tw>
2023-04-13 10:26:28 +0800

    尾端空格去除

--- vs-work1-week.cpp
+++ vs-work1-week.cpp
@@ -13,7 +13,7 @@ int main() {
     string stile;
     int x = 1, temp[10]={0};
     char ctile[10]={0};
-    cout << "請輸入整數:";
+    
     cin >> stile;
     
      for (int i = 0; i < size(stile); i++) {
@@ -21,11 +21,13 @@ int main() {
         temp[i] =ctile[i]-'0';
      }
      
-
      for (int i = 0; i < 5; i++) {
          for (int j = 0; j < size(stile); j++) {
              cout << Card[i][temp[j]];
-             cout << " ";
+             if (j == (size(stile) - 1)) { 
+                 break;
+             }else cout << " ";
+            
          }
         cout << '\n';
     }
