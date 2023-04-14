dd8dbce34cbe5920ab7d1c2ccfacbf3211f64381
日平 趙 <4b1g0034@office.stust.edu.tw>
2023-04-14 11:29:17 +0800

    修正ITSA要求

--- vs-work1-week.cpp
+++ vs-work1-week.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 int main() {
 	int timex1, timey1, timex2, timey2, temp1, temp2, temp, many, tempcopy;
-	cout << "請輸入開始停車的時間(24小時):";
+	//cout << "請輸入開始停車的時間(24小時):";
 	cin >> timex1 >> timey1;
-	cout << "請輸入離開停車的時間(24小時):";
+	//cout << "請輸入離開停車的時間(24小時):";
 	cin >> timex2 >> timey2;
 	temp1 = timex1 * 60 + timey1;
 	temp2 = timex2 * 60 + timey2;
@@ -18,19 +18,23 @@ int main() {
 		
 		case 1: case 2: case 3: case 4:
 			many = 30 * tempcopy;
-			cout << "你的停車費是: " << many << endl;
+			//cout << "你的停車費是: " << many << endl;
+			cout << many << endl;
 			break;
 		case 5: case 6: case 7: case 8:
 			many = 30 * 4 + 40 * (tempcopy-4) ;
-			cout << "你的停車費是: " << many << endl;
+			//cout << "你的停車費是: " << many << endl;
+			cout << many << endl;
 			break;
 		case 0: 
 			many = 0;
-			cout << "你的停車費是: " << many << endl;
+			//cout << "你的停車費是: " << many << endl;
+			cout << many << endl;
 			break;
 		default:
 			many = 30 * 4 + 40 * 4 + 60 * (tempcopy-8);
-			cout << "你的停車費是: " << many << endl;
+			//cout << "你的停車費是: " << many << endl;
+			cout << many << endl;
 			break;
 	}
 }
